84d6df0462d0a9c6ccab5357d0ea3a814fe0a26e
Egor Kazantsev <egor-kazantsev@bk.ru>
2023-01-27 11:08:47 -0500

    last update

--- src/Sync/Handlers/AuthHandler.php
+++ src/Sync/Handlers/AuthHandler.php
@@ -4,25 +4,30 @@ declare(strict_types=1);
 
 namespace Sync\Handlers;
 
+use AmoCRM\Client\AmoCRMApiClient;
+use Exception;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Sync\Kommo\AuthService;
 
-class AuthHandler implements RequestHandlerInterface
+class AuthHandler extends AuthService implements RequestHandlerInterface
 {
+    /**
+     * @throws Exception
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $name = $request->getQueryParams()['name'];
-        if (!isset(json_decode(file_get_contents('./tokens.json'), true)[$name])) {
+        $a = $request->getQueryParams()['name'];
+        if (!isset(json_decode(file_get_contents('./tokens.json'), true)[$a])) {
             (new AuthService())->auth();
             return new JsonResponse([
                 ['name' => $_SESSION['name']]
             ]);
         }
         return new JsonResponse([
-            ['name' => $name]
+            ['name' => $a]
         ]);
     }
 }
